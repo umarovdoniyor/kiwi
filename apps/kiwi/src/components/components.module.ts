f5d2583159744bcdd1410fb9f63252cd81177443
Danny <mr.danny951008@gmail.com>
2026-02-24 02:22:20 +0900

    feat: create components related modules

--- apps/kiwi/src/components/components.module.ts
+++ apps/kiwi/src/components/components.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { MemberModule } from './member/member.module';
 import { ProductModule } from './product/product.module';
+import { AuthModule } from './auth/auth.module';
+import { LikeModule } from './like/like.module';
+import { ViewModule } from './view/view.module';
+import { FollowModule } from './follow/follow.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [MemberModule, ProductModule],
+  imports: [
+    MemberModule,
+    AuthModule,
+    ProductModule,
+    OrderModule,
+    LikeModule,
+    ViewModule,
+    FollowModule,
+  ],
 })
 export class ComponentsModule {}
