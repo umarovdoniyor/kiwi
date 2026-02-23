e1cc5f619d95826f1f955efd631e390b260abb2f
Danny <mr.danny951008@gmail.com>
2026-02-23 22:54:29 +0900

    feat: configure .env variables

--- apps/kiwi-batch/src/kiwi-batch.module.ts
+++ apps/kiwi-batch/src/kiwi-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { KiwiBatchController } from './kiwi-batch.controller';
 import { KiwiBatchService } from './kiwi-batch.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [KiwiBatchController],
   providers: [KiwiBatchService],
 })
--- apps/kiwi-batch/src/main.ts
+++ apps/kiwi-batch/src/main.ts
@@ -3,6 +3,6 @@ import { KiwiBatchModule } from './kiwi-batch.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(KiwiBatchModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.PORT_BATCH ?? 3008);
 }
 bootstrap();
